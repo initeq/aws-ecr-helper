e179c802fb4f90c1d4662cde1cf8401c65bc66ef
Deni Bertovic <deni@denibertovic.com>
2024-02-23 21:27:28 +0100

    Updates aws ecr login command

--- refresh_credentials.sh
+++ refresh_credentials.sh
@@ -3,7 +3,7 @@ set -eux
 
 DOCKER_REGISTRY_SERVER=https://${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com
 DOCKER_USER=AWS
-DOCKER_PASSWORD=`aws ecr get-login --region ${AWS_REGION} --registry-ids ${AWS_ACCOUNT} | cut -d' ' -f6`
+DOCKER_PASSWORD=`aws ecr get-login-password --region ${AWS_REGION}`
 echo "DOCKER_REGISTRY_SERVER=$DOCKER_REGISTRY_SERVER"
 echo "DOCKER_USER=$DOCKER_USER"
 
