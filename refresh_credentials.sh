970cdf7f04a17d91fe3dd996b511383d9c2305c6
Deni Bertovic <deni@denibertovic.com>
2024-02-23 20:49:35 +0100

    Breaking change: support multiple namespaces

--- refresh_credentials.sh
+++ refresh_credentials.sh
@@ -6,9 +6,17 @@ DOCKER_USER=AWS
 DOCKER_PASSWORD=`aws ecr get-login --region ${AWS_REGION} --registry-ids ${AWS_ACCOUNT} | cut -d' ' -f6`
 echo "DOCKER_REGISTRY_SERVER=$DOCKER_REGISTRY_SERVER"
 echo "DOCKER_USER=$DOCKER_USER"
-kubectl delete secret --namespace $TARGET_NAMESPACE aws-registry || true
-kubectl create secret --namespace $TARGET_NAMESPACE docker-registry aws-registry \
---docker-server=$DOCKER_REGISTRY_SERVER \
---docker-username=$DOCKER_USER \
---docker-password=$DOCKER_PASSWORD \
---docker-email=no@email.local
+
+IFS=' ' read -r -a TARGET_NAMESPACES_ARR <<< "$TARGET_NAMESPACES"
+
+for i in "${TARGET_NAMESPACES_ARR[@]}"
+do
+    :
+    echo "$(date): Updating secret for namespace - $i"
+    kubectl delete secret --namespace $i aws-registry || true
+    kubectl create secret --namespace $i docker-registry aws-registry \
+    --docker-server=$DOCKER_REGISTRY_SERVER \
+    --docker-username=$DOCKER_USER \
+    --docker-password=$DOCKER_PASSWORD \
+    --docker-email=no@email.local
+done
